beff55597d11b491fd59e9997acf6db975613e52
yunusjalman <79404390+yunusjalman@users.noreply.github.com>
2021-03-11 00:50:43 +0000

    oi.py

--- userbot/modules/oi.py
+++ userbot/modules/oi.py
@@ -2,27 +2,27 @@ from time import sleep
 from userbot.events import register
 
 
-@register(outgoing=True, pattern='^.alvin(?: |$)(.*)')
+@register(outgoing=True, pattern='^.yunus(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
-    await typew.edit("`Hai Perkenalkan Namaku Alvin`")
+    await typew.edit("`Hallo Perkenalkan Yah Nama Gw Yunus`")
     sleep(3)
-    await typew.edit("`17 Tahun`")
+    await typew.edit("`Umur 19 tahun`")
     sleep(1)
-    await typew.edit("`Tinggal Dibatam, Salam Kenal:)`")
+    await typew.edit("`Asal Dari Padang Salam Kenal Yah:)`")
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern='^.sayang(?: |$)(.*)')
+@register(outgoing=True, pattern='^.tolol(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
     await typew.edit("`Cuma Mau Bilang`")
     sleep(3)
-    await typew.edit("`Aku Sayang Kamu`")
+    await typew.edit("`Lu tolol Goblok`")
     sleep(1)
-    await typew.edit("`I LOVE YOU 💞`")
+    await typew.edit("`DASAR AUTISSS`")
 # Create by myself @localheart
 
 
@@ -32,7 +32,7 @@ async def typewriter(typew):
     sleep(3)
     await typew.edit("`Apapun Yang Terjadi`")
     sleep(3)
-    await typew.edit("`Tetaplah Bernapas`")
+    await typew.edit("`Jangan Bernafas`")
     sleep(1)
-    await typew.edit("`Dan Selalu Bersyukur`")
+    await typew.edit("`Bunuh Diri Aja`")
 # Create by myself @localheart
